8de3af30a0b7bbb88a552d29479fd0d11ad31af6
GabeMKHEXETER <gmkh201@exeter.ac.uk>
2026-02-18 12:27:49 +0000

    task 3 completed

--- StringFormatApp.java
+++ StringFormatApp.java
@@ -1,4 +1,5 @@
 //used Jshell for majority of Task 3
+//java.until.Formatter ?
 
 public class StringFormatApp{
     public static void main(String[] args){
@@ -10,9 +11,18 @@ public class StringFormatApp{
         System.out.println("book (object) == book (string): "+ (s1==s2));
         System.out.println("book (string) == book (string): " + (s2==s3));
 
-        int age = Integer.parseInt(args[0]);
-        float height = Float.parseFloat(args[1]);
-        //String name = String.parseStr(args[2]); //how to without scanner..? :<
-        System.out.printf("Age: %d Height: %d%n", age, height);
+        System.out.printf("Name: %1$20s Age: %2$3s Height: %3$4s%n", args[0], args[1], args[2]); //I remembered :D (format is close enough..)
+
+        //space height :D (3.3)
+        float height = Float.parseFloat(args[2]);
+        double spaceHeight = height * 1.023 * 100; //2.3% increase (in cm)
+        String formattedHeight = String.format("%.02f", spaceHeight); //had to adjust here, %f was being strange 
+        System.out.printf("My space height would be " + formattedHeight +"cm %n");
+
+        //space age (3.4)
+        int age = Integer.parseInt(args[1]); 
+        double spaceAge = age / 1.88; 
+        System.out.printf("On mars, I would be %.0f%n",spaceAge);
+
     }
 }
